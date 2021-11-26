bc775f2c9f0a147bd5732ee328c5b4f679e60f99
jejkobb <hansesjakob@gmail.com>
2021-11-26 12:05:40 +0200

    nicer python script

--- dfs.py
+++ dfs.py
@@ -6,27 +6,52 @@ fill_symbol = " "
 draw_symbol = 2
 
 directions = [
-    [0, 1],
-    [1, 0],
-    [0,-1],
-    [-1,0]
+    [ 0, 1],
+    [ 1, 0],
+    [ 0,-1],
+    [-1, 0],
+    [ 1, 1],
+    [-1, 1],
+    [-1,-1],
+    [ 1,-1]
 ]
+
 grid = []
 for row in range(0,h):
     grid.append([]);
     for col in range(0,w):
-        if(random.randint(0,2) == 1):
+        if(random.randint(0,3) == 1):
             grid[row].append(draw_symbol);
         else:
             grid[row].append(fill_symbol);
+
 def printGrid(g):
     for row in g:
         s = ""
         for col in row:
             s+=str(col)
         print(s)
+
+def removeLoners(matrix, row, col):
+    if matrix[row][col] != draw_symbol:
+        return
+    for i in directions:
+        try:
+            if matrix[row+i[0]][col+i[1]] != fill_symbol:
+                return
+        except:
+            matrix[row][col] = fill_symbol
+            return
+    matrix[row][col] = fill_symbol
+        
+
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        removeLoners(grid, row, col)
+
 printGrid(grid)
 print("-"*w)
+
 def dfs(matrix, row, col):
     if row < 0 or col < 0 or row >= h or col >= w:
         return 0
@@ -40,6 +65,7 @@ def dfs(matrix, row, col):
             for a in arr:
                 __size.append(a)
     return __size
+
 longest_arr = []
 for row in range(len(grid)):
     for col in range(len(grid[row])):
